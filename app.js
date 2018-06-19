afc8eac36bc7c54f4bd2bf9e72c8fbfd02c71a05
zach <zach@zachs-MacBook-Air.local>
2018-06-18 20:58:32 -0500

    removed useless dependency

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').load();
 require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 const { authRoutes, userRoutes, postRoutes } = require('./routes');
 
